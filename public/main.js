9dd3ed1eaf9ad8abeb1b8dab7fce4703da2a4c20
Rubens TORRES-LACAZE <rubens.torres-lacaze@telecom-sudparis.eu>
2025-02-15 18:11:19 +0100

    Added username display to client

--- public/main.js
+++ public/main.js
@@ -75,7 +75,17 @@ function clearMessages() {
 
 /* ---------------------- Event handling ---------------------------*/
 
+// Après la connexion WebSocket (const socket = io();)
+socket.on("connect", () => {
+  // Demander l'username au serveur
+  socket.emit("get username");
+});
 
+// Réception de l'username
+socket.on("send username", (username) => {
+  const usernameInput = document.getElementById("username");
+  usernameInput.value = username; // Afficher l'username
+});
 
 // When receiveing whole message stack
 socket.on('message stack broadcast', (messages) => {
